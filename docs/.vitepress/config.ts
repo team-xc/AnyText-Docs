447431eb37881df667654ded7fb4d91561a3839d
Lxc <alone.lxc@gmail.com>
2023-02-15 05:37:28 +0800

    初始化文档

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
+  title: '开发文档 | AnyText',
+  description: 'This is a super powerful text processing tool.',
+  appearance: 'dark',
+  base: '/docs/',
+  lang: 'zh-CN'
+})
