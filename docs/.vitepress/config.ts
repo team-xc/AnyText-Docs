0ecc5943338bcde5cc9bcefca46d3d3520e57571
Lxc <alone.lxc@gmail.com>
2023-02-15 08:52:52 +0800

    优化目录结构

--- docs/.vitepress/config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vitepress'
-
-export default defineConfig({
-  title: '开发文档 | AnyText',
-  description: 'This is a super powerful text processing tool.',
-  appearance: 'dark',
-  base: '/docs/',
-  lang: 'zh-CN'
-})
